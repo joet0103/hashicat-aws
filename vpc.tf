268c3d820b73a3acd55dedaebe332e15daa2fe73
Your Name <you@example.com>
2020-06-24 09:01:28 +0000

    Update

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,6 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.44.0"
   
-  cidr_block           = "10.10.0.0/16"
-  enable_dns_hostnames = true
+  name = "test-vpc"
+  cidr = "10.10.0.0/16"
 }
